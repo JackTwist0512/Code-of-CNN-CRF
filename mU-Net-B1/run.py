d26d859453f4a143faae795400fee25a30d7e349
JackTwist0512 <41134183+JackTwist0512@users.noreply.github.com>
2020-07-22 20:22:21 +0800

    Add files via upload

---
+++ mU-Net-B1/run.py
@@ -0,0 +1,15 @@
+from main import*
+from evaluation import*
+
+main(26,5)
+# for i in range(4,6):
+#     main(26,i)
+# for i in range(18,21):
+#      main(i,0)
+
+
+# for i in range(0,21):
+#       n = [0, 1, 2]
+#       evaluation(i, n[1])
+#       evaluation(i, n[2])
+#       evaluation(i, n[0])
